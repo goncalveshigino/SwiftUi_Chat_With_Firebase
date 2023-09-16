d8e931118b1115a4ca94c9c8953548108b6429f0
goncalveshigino <goncalveshigino20@gmail.com>
2023-09-16 23:33:47 +0100

    Initial Commit

---
+++ SwiftMessagerClone/SwiftMessagerCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMessagerCloneApp.swift
+//  SwiftMessagerClone
+//
+//  Created by Goncalves Higino on 16/09/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMessagerCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
